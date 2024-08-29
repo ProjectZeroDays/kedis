44d170f1686fb844fea42ed554d2c30fb94a75be
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-29 16:42:27 +0000

    update

--- app/utils/tcp.ts
+++ app/utils/tcp.ts
@@ -1,23 +1,19 @@
 import * as net from "net";
 
 export default class TCP {
-    socket: net.Socket;
     private port: number;
     private host: string;
 
     constructor (port: number, host: string) {
         this.port = port;
         this.host = host;
-        this.socket = new net.Socket();
     }
 
     send(msg: string) {
-        this.socket.connect(this.port, this.host);
-        this.socket.write(msg);
-        this.socket.end();
+        const socket = new net.Socket();
+        socket.connect(this.port, this.host);
+        socket.write(msg);
+        socket.end();
     }
 
-    close() {
-        this.socket.end();
-    }
 }
