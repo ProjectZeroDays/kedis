d485a0efdf89cd19a15ab42dd995d0c8f82269df
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-29 19:16:09 +0000

    update

--- app/utils/parser.ts
+++ app/utils/parser.ts
@@ -90,10 +90,12 @@ export default class Parser {
     let lastUnique: false | string = false;
 
     const [numOfArgs, commandLength, cmd, ...params] = args;
-    const command = cmd.toUpperCase() as Command;
+    let command = cmd as Command;
 
     if (availableCommands.indexOf(command) === -1 && !commands[command])
       return undefined;
+
+    command = command.toUpperCase() as Command;
 
     if (["GET", "SET", "ECHO", "PING"].includes(command)) {
       for (const p of params) {
