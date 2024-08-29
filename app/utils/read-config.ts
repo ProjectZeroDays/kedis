8067fc78f3ab63c497f4386f3541fbdd110993dc
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-29 13:51:46 +0000

    update

--- app/utils/read-config.ts
+++ app/utils/read-config.ts
@@ -1,5 +1,22 @@
 export default function readConfig() {
-    const args = require('minimist')(process.argv.slice(2));
+    const args = process.argv.slice(2);
+    const config = parseArgs(args);
+
+    return config;
+}
+
+function parseArgs(args: string[]) {
+    const config: Record<string, string> = {};
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i];
+        const value = args[i + 1];
+        config[key] = value;
+    }
+
+    return validateConfig(config);
+}
+
+function validateConfig(args: Record<string, string>) {
     let dir = args['dir'];
     let dbfilename = args['dbfilename'];
 
