4013597d5bd22fa30f84ac5192ce82967efb3162
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-29 21:28:25 +0000

    update

--- app/utils/commands.ts
+++ app/utils/commands.ts
@@ -197,6 +197,9 @@ class Commands {
     };
 
     store.replicas.forEach((r) => r[1].on("data", listener));
+    store.replicas.forEach((r) =>
+      r[1].write(Parser.listResponse(["REPLCONF", "GETACK", "*"]))
+    );
 
     setTimeout(() => {
       store.replicas.forEach((r) => r[1].off("data", listener));
