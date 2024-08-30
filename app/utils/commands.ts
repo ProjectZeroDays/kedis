feff7ca2f8ad0ee1badd454e244235f637bc1fd3
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-30 15:35:18 +0000

    update

--- app/utils/commands.ts
+++ app/utils/commands.ts
@@ -352,6 +352,7 @@ class Commands {
 
       const ids = stream.entries.map((e) => e[0]);
       const startId = ids.indexOf(id);
+      console.log("Read stream:", streamKey, id);
 
       if (id === "0-0") {
         return reads.push(Parser.streamXResponse(stream));
