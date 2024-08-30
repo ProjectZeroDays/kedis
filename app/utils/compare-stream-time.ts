068afc3e3cafa01a7262a8fa36025bfe953310a7
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-08-30 12:45:35 +0000

    update

--- app/utils/compare-stream-time.ts
+++ app/utils/compare-stream-time.ts
@@ -4,7 +4,8 @@ export default function compareStreamTime(a: string, b: string) {
     const aTime = a.split("-").map(i => parseInt(i));
     const bTime = b.split("-").map(i => parseInt(i));
 
-    if (aTime[0] >= bTime[0]) return false;
+    if (aTime[0] > bTime[0]) return false;
+    if (aTime[0] === bTime[0] && aTime[1] >= bTime[1]) return false;
 
     return true;
 }
